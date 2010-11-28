4bc8a231b5507efd4caf1255625b7995f2f77f37
Alberto Zaccagni <montecristoh@gmail.com>
2010-11-28 15:01:47 +0100

    added the part for the task hour loggin, still need to make it work... :P

--- rmr.js
+++ rmr.js
@@ -20,8 +20,7 @@ var requestHeaders = {
 // Recupero tutte le issue aperte e assegnate all'utente
 var reqIssues = redmine.request('GET', '/issues.xml?assigned_to_id=me', requestHeaders);
 
-// Template per generare la lista con Zenity
-var issuesListTpl = '/usr/bin/zenity --list --width=600 --height=500 --title="Elenco delle issue che ti sono assegnate" --column="#" --column="Progetto" --column="Titolo" ';
+var issuesListTemplate = '/usr/bin/zenity --list --width=600 --height=500 --title="Elenco delle issue che ti sono assegnate" --column="#" --column="Progetto" --column="Titolo" ';
 
 reqIssues.on('response', function(response) {
 	
@@ -35,7 +34,7 @@ reqIssues.on('response', function(response) {
   }).on('end', function() {
     parser.on('end', function(result) {
       // Compongo la lista delle issue
-      issuesList = issuesListTpl;
+      issuesList = issuesListTemplate;
       var issues = result.issue;
       for (var i in issues) {
         // Strippo via i doppi apici poiché sono usati da Zenity come enclosure
@@ -43,7 +42,15 @@ reqIssues.on('response', function(response) {
       }
 
       // Mostra la lista
-      child = exec(issuesList, function(error, stdout, stderr) {});
+      child = exec(issuesList, function(error, stdout, stderr) {
+    	  var issueId = stdout.replace("\n", "");
+    	  exec('/usr/bin/zenity --question --text "Vuoi loggare un\'ora sul task '+issueId+'?"; echo $?', function(error, stdout, stderr){
+		      if(stdout.replace("\n", "")===0){
+		    	  //user pressed yes 
+		    	  exec('curl -v -H "Content-Type:text.json" -X PUT --data \'{"spent_time": "3.0"}\' -u cfg.getConfig().user:cfg.getConfig().pass http://tracker.nextre.it/issues/'+issueId+'.json', function(error, stdout, stderr){});
+		      }
+    	  });
+      });
     }).parseString(body);
   });
 }).end();
